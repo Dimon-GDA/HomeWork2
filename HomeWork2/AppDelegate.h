47885bbd94e4a0d2e1150b5e63e209d9f17673fa
Dimon-GDA <dimon.gda@gmail.com>
2013-02-11 00:30:19 +0400

    Initial Commit

---
+++ HomeWork2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HomeWork2
+//
+//  Created by admin on 2/11/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
